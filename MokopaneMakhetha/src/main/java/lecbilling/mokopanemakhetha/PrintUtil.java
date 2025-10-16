0fe6b50e745261a9907ee721925071744b48f3a4
Mokopane Makhetha <mokopanem38@gmail.com>
2025-10-16 11:21:50 +0200

    Add files via upload

---
+++ MokopaneMakhetha/src/main/java/lecbilling/mokopanemakhetha/PrintUtil.java
@@ -0,0 +1,22 @@
+package lecbilling.mokopanemakhetha;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+public class PrintUtil {
+    public static void showAlert(String title, String message, Alert.AlertType type) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    public static boolean showConfirmation(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        return alert.showAndWait().orElse(ButtonType.CANCEL) == ButtonType.OK;
+    }
+}
---
+++ MokopaneMakhetha/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lecbilling.mokopanemakhetha {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lecbilling.mokopanemakhetha to javafx.fxml;
+    exports lecbilling.mokopanemakhetha;
+}
